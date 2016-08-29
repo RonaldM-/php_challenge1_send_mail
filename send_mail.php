d3e08cdf009ad916176c73d3255c0d102340c4a4
RonaldM- <ronald.marcel.mr@gmail.com>
2016-08-30 00:19:26 +0200

    Php - Mise en place du $headers pour l'intégration Cc et CCi alias Bcc.Je ne sais pas si celà est fonctionnel pas  de sendmail

--- send_mail.php
+++ send_mail.php
@@ -1,6 +1,9 @@
 <?php
-
+$email= $_POST['email'];
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $botbot = 'botfrom@botland.fu';
+    $cci = $_POST['emailcci'];
+    $bcc = $_POST['emailcc'];
 
     $to = $_POST["emailto"];
 
@@ -9,8 +12,13 @@
     $message = $_POST["message"];
 
     $message = wordwrap($message, 70, "\r\n");
-    $headers = $botbot;
-
+    $headers .= "From: $botbot" . "\r\n";
+    $headers .= "Cc: $cc". '\r\n';
+    $headers .= "Bcc: $bcc". "\r\n";
 mail($to, $subject, $message, $headers);
-
+    echo "I did my job, your email has been sent!";
+}
+else {
+    echo "L'adresse suivante " .'"'.$email.'"'. " est invalide. <br><a href='index.php' role='button'><button>Try again , please!</button></a>";
+}
 ?>
